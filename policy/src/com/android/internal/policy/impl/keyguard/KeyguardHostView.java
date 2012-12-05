d8d5fbcc936738d14a00f3c7f72cab60c4b41b23
Winson Chung <winsonc@google.com>
2012-12-05 15:58:43 -0800

    Revert "Saving and restoring current page when rotating in lockscreen. (Bug 7568412)"
    This reverts commit 6cbc2e81742039969e081a9eed02060fb8789e25.

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -85,7 +85,6 @@ public class KeyguardHostView extends KeyguardViewBase {
     private boolean mEnableFallback; // TODO: This should get the value from KeyguardPatternView
     private SecurityMode mCurrentSecuritySelection = SecurityMode.Invalid;
     private int mAppWidgetToShow;
-    private int mPreviousWidgetPage = -1;
 
     private boolean mCheckAppWidgetConsistencyOnBootCompleted = false;
     private boolean mCleanupAppWidgetsOnBootCompleted = false;
@@ -970,7 +969,6 @@ public class KeyguardHostView extends KeyguardViewBase {
 
     public void clearAppWidgetToShow() {
         mAppWidgetToShow = AppWidgetManager.INVALID_APPWIDGET_ID;
-        mPreviousWidgetPage = -1;
     }
 
     @Override
@@ -1358,22 +1356,10 @@ public class KeyguardHostView extends KeyguardViewBase {
            showAppropriateWidgetPage();
         }
     };
-    Runnable mOnRestoreUpdatePageRunnable = new Runnable() {
-        @Override
-        public void run() {
-            mSwitchPageRunnable.run();
-
-            // We need to update the alpha's of the hints and content after setting the current page
-            mViewStateManager.showUsabilityHints();
-        };
-    };
-
 
     static class SavedState extends BaseSavedState {
         int transportState;
         int appWidgetToShow = AppWidgetManager.INVALID_APPWIDGET_ID;
-        // Note the appWidgetContainerPage is only used if the appWidgetToShow is invalid
-        int appWidgetContainerPage = -1;
 
         SavedState(Parcelable superState) {
             super(superState);
@@ -1383,7 +1369,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             super(in);
             this.transportState = in.readInt();
             this.appWidgetToShow = in.readInt();
-            this.appWidgetContainerPage = in.readInt();
         }
 
         @Override
@@ -1391,7 +1376,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             super.writeToParcel(out, flags);
             out.writeInt(this.transportState);
             out.writeInt(this.appWidgetToShow);
-            out.writeInt(this.appWidgetContainerPage);
         }
 
         public static final Parcelable.Creator<SavedState> CREATOR
@@ -1413,7 +1397,6 @@ public class KeyguardHostView extends KeyguardViewBase {
         SavedState ss = new SavedState(superState);
         ss.transportState = mViewStateManager.getTransportState();
         ss.appWidgetToShow = mAppWidgetToShow;
-        ss.appWidgetContainerPage = mAppWidgetContainer.getCurrentPage();
         return ss;
     }
 
@@ -1428,8 +1411,7 @@ public class KeyguardHostView extends KeyguardViewBase {
         super.onRestoreInstanceState(ss.getSuperState());
         mViewStateManager.setTransportState(ss.transportState);
         mAppWidgetToShow = ss.appWidgetToShow;
-        mPreviousWidgetPage = ss.appWidgetContainerPage;
-        post(mOnRestoreUpdatePageRunnable);
+        post(mSwitchPageRunnable);
     }
 
     @Override
@@ -1485,9 +1467,6 @@ public class KeyguardHostView extends KeyguardViewBase {
                 }
             }
             mAppWidgetToShow = AppWidgetManager.INVALID_APPWIDGET_ID;
-        }
-        if (mPreviousWidgetPage > -1) {
-            return mPreviousWidgetPage;
         }
         // if music playing, show transport
         if (isMusicPlaying) {
