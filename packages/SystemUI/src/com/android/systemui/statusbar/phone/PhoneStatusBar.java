f2ed0a2dbe55a76c45137e3b21abf3b3d08d6f6c
Thiago Vinícius Freire de Araújo Ribeiro <thiagovfar@gmail.com>
2012-12-25 20:10:09 -0300

    Revert "FW - User select Tablet Mode [2/2]"
    This reverts commit 9946f4d9215d7b40991a3e0529646616d6a1acdc.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -167,6 +167,7 @@ public class PhoneStatusBar extends BaseStatusBar {
     int mIconHPadding = -1;
     Display mDisplay;
     Point mCurrentDisplaySize = new Point();
+    int mCurrentUIMode = 0; 
 
     IDreamManager mDreamManager;
 
