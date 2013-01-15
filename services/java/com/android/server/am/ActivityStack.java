495948de84b923a48fd2917b60c0187151cc9af2
LorD ClockaN <davor@losinj.com>
2013-01-15 17:36:18 +0100

    Revert "ActivityTrigger:  New class to invoke when activity starts/resumes"
    This reverts commit 8f83f97dca03c230c5a9ad9337f12eb6943548eb.
    
    Conflicts:
    	services/java/com/android/server/am/ActivityStack.java

--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -56,14 +56,12 @@ import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
-import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.util.EventLog;
 import android.util.Log;
 import android.util.Slog;
 import android.view.Display;
 import android.view.WindowManagerPolicy;
-import com.android.internal.app.ActivityTrigger;
 
 import java.io.IOException;
 import java.lang.ref.WeakReference;
@@ -310,17 +308,7 @@ final class ActivityStack {
         }
     }
 
-    private static final ActivityTrigger mActivityTrigger;
-
     private final PowerManagerService mPm;
-
-    static {
-        if (SystemProperties.QCOM_HARDWARE) {
-            mActivityTrigger = new ActivityTrigger();
-        } else {
-            mActivityTrigger = null;
-        }
-    }
 
     final Handler mHandler = new Handler() {
         //public Handler() {
@@ -1488,10 +1476,6 @@ final class ActivityStack {
 
         if (DEBUG_SWITCH) Slog.v(TAG, "Resuming " + next);
 
-        if (mActivityTrigger != null) {
-            mActivityTrigger.activityResumeTrigger(next.intent);
-        }
-
         // If we are currently pausing an activity, then don't do anything
         // until that is done.
         if (mPausingActivity != null) {
@@ -2504,9 +2488,6 @@ final class ActivityStack {
             final int userId = aInfo != null ? UserHandle.getUserId(aInfo.applicationInfo.uid) : 0;
             Slog.i(TAG, "START u" + userId + " {" + intent.toShortString(true, true, true, false)
                     + "} from pid " + (callerApp != null ? callerApp.pid : callingPid));
-            if (mActivityTrigger != null) {
-                mActivityTrigger.activityStartTrigger(intent);
-            }
         }
 
         ActivityRecord sourceRecord = null;
