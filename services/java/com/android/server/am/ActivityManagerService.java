1edec014278da699e1ec7a0b3f1b08a5f2637069
LorD ClockaN <davor@losinj.com>
2013-02-13 18:28:02 +0100

    Quiet down a lot of logging.
    Also fix a little problem where the USER_STARTED broadcasts
    were not being sent as ordered broadcasts(!).
    
    Change-Id: I3aa3e0a9b3900967cdd2d115ee103371b0a50c41
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/usb/StorageNotification.java

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -23,7 +23,6 @@ import com.android.internal.R;
 import com.android.internal.app.ThemeUtils;
 import com.android.internal.os.BatteryStatsImpl;
 import com.android.internal.os.ProcessStats;
-import com.android.internal.widget.LockPatternUtils;
 import com.android.server.AttributeCache;
 import com.android.server.IntentResolver;
 import com.android.server.ProcessMap;
@@ -7966,7 +7965,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                             }
                         }, 0, null, null,
                         android.Manifest.permission.INTERACT_ACROSS_USERS,
-                        false, false, MY_PID, Process.SYSTEM_UID, UserHandle.USER_ALL);
+                        true, false, MY_PID, Process.SYSTEM_UID, UserHandle.USER_ALL);
             } finally {
                 Binder.restoreCallingIdentity(ident);
             }
@@ -14300,7 +14299,7 @@ public final class ActivityManagerService extends ActivityManagerNative
                                 }
                             }, 0, null, null,
                             android.Manifest.permission.INTERACT_ACROSS_USERS,
-                            false, false, MY_PID, Process.SYSTEM_UID, UserHandle.USER_ALL);
+                            true, false, MY_PID, Process.SYSTEM_UID, UserHandle.USER_ALL);
                 }
             }
         } finally {
@@ -14523,7 +14522,7 @@ public final class ActivityManagerService extends ActivityManagerNative
             long ident = Binder.clearCallingIdentity();
             try {
                 // We are going to broadcast ACTION_USER_STOPPING and then
-                // once that is down send a final ACTION_SHUTDOWN and then
+                // once that is done send a final ACTION_SHUTDOWN and then
                 // stop the user.
                 final Intent stoppingIntent = new Intent(Intent.ACTION_USER_STOPPING);
                 stoppingIntent.addFlags(Intent.FLAG_RECEIVER_REGISTERED_ONLY);
