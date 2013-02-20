7326e7ea7977bb13c414170d8745650009706fc3
LorD ClockaN <davor@losinj.com>
2013-02-20 21:39:21 +0100

    Revert "Framework: QuickRecord Tile (2/3) - AOKP"
    This reverts commit 4e62ccb90e0506cbc97df9d56fae0486aba8785b.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
@@ -304,10 +304,6 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     private RefreshCallback mTorchCallback;
     private State mTorchState = new State();
 
-    private QuickSettingsTileView mQuickRecordTile;
-    private RefreshCallback mQuickRecordCallback;
-    private State mQuickRecordState = new State();
-
     private QuickSettingsTileView mWifiTetherTile;
     private RefreshCallback mWifiTetherCallback;
     private State mWifiTetherState = new State();
@@ -1315,21 +1311,6 @@ class QuickSettingsModel implements BluetoothStateChangeCallback,
     void refreshTorchTile() {
         if (mTorchTile != null) {
             onTorchChanged();
-        }
-    }
-
-    // Quick Record
-    void addQuickRecordTile(QuickSettingsTileView view, RefreshCallback cb) {
-        mQuickRecordTile = view;
-        mQuickRecordCallback = cb;
-        mQuickRecordCallback.refreshView(mQuickRecordTile, mQuickRecordState);
-    }
-
-    void setQuickRecordTileInfo(String playStateName, Integer playStateIcon) {
-        if (mQuickRecordCallback != null) {
-            mQuickRecordState.label = playStateName;
-            mQuickRecordState.iconId = playStateIcon;
-            mQuickRecordCallback.refreshView(mQuickRecordTile, mQuickRecordState);
         }
     }
 
