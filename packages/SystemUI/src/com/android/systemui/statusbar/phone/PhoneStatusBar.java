36790d60f255b41486afbf9708be86925e000b6a
LorD ClockaN <davor@losinj.com>
2013-03-15 23:05:49 +0100

    Fix build - PIE derp

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2105,7 +2105,6 @@ public class PhoneStatusBar extends BaseStatusBar {
 
     public void topAppWindowChanged(boolean showMenu) {
         mTransparencyManager.update();
-        mStatusBarView.updateBackgroundAlpha();
         if (mPieControlPanel != null)
             mPieControlPanel.setMenu(showMenu);
         if (DEBUG) {
