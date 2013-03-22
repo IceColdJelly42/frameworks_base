db887d0dce6d9ce80d4c4de3d44767ac37d825df
maxwen <max.weninger@gmail.com>
2013-03-22 22:50:41 +0100

    FW: Option to hide Camera widget from lockscreen (1/2) - cleanup
    Change-Id: Ic43f223a895b094e4ce8a248269a337376c8d8a4

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3488,12 +3488,12 @@ public final class Settings {
         public static final String MISSED_CALL_BREATH = "missed_call_breath";
 
         /**
-         * Allows to disabel Camera widget in locskcreen
-         * 0 = use system default (show)
+         * Allows to disable Camera widget in locskcreen
+         * 0 = show camera default
          * 1 = hide camera
          * @hide
          */
-        public static final String CAMERA_WIDGET_HIDE = "camera_widget_show";
+        public static final String CAMERA_WIDGET_HIDE = "camera_widget_hide";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
