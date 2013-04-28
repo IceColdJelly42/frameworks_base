162e5bee46910d5e96456a8307531e458f2d231f
LorD ClockaN <davor@losinj.com>
2013-04-28 23:08:22 +0200

    Revert "Use boolean instead of Int"
    This reverts commit c304f07590530c7b258dbb159bb9c68b1c7ce724.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1313,8 +1313,8 @@ public class PhoneStatusBar extends BaseStatusBar {
     }
 
     private void updateStatusBarVisibility() {
-        if (Settings.System.getBoolean(mContext.getContentResolver(),
-                Settings.System.AUTO_HIDE_STATUSBAR, false)) {
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.AUTO_HIDE_STATUSBAR, 0) == 1) {
             Settings.System.putInt(mContext.getContentResolver(),
                     Settings.System.HIDE_STATUSBAR,
                     (mNotificationData.size() == 0) ? 1 : 0);
