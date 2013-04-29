74a60c761a300a637c23a8d0d6cecc7c24709654
maxwen <max.weninger@gmail.com>
2013-04-30 00:19:15 +0200

    fix for auto-close 3-dot menu
    Change-Id: I0fb0414f565696b810a8375005b9cb4702bdb600
    
    Conflicts:
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -529,6 +529,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     boolean mHomePressed;
     boolean mHomeLongPressed;
     boolean mMenuLongPressed;
+    boolean mMenuWasInjected = false;
     boolean mBackLongPressed;
     boolean mAppSwitchLongPressed;
     Intent mHomeIntent;
@@ -1062,6 +1063,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     break;
                 case KEY_ACTION_MENU:
                     triggerVirtualKeypress(KeyEvent.KEYCODE_MENU);
+                    mMenuWasInjected = true;
                     break;
                 case KEY_ACTION_APP_SWITCH:
                     sendCloseSystemWindows(SYSTEM_DIALOG_REASON_RECENT_APPS);
@@ -2470,7 +2472,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         if (DEBUG_INPUT) {
             Log.d(TAG, "interceptKeyTi keyCode=" + keyCode + " down=" + down + " repeatCount="
                     + repeatCount + " keyguardOn=" + keyguardOn + " mHomePressed=" + mHomePressed
-                    + " canceled=" + canceled);
+                    + " canceled=" + canceled + " mIsVirtualKeypress=" + mIsVirtualKeypress);
         }
 
         // If we think we might have a volume down & power/volume-up key chord on the way
@@ -2670,6 +2672,18 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 }
                 if (canceled)
                     return -1;
+
+
+                // maxwen: special key injected KEYCODE_MENU
+                // ignore the up event that comes after this virtual one
+                // else the menu will close if 3-dot menu is used
+                if (!mIsVirtualKeypress && mMenuWasInjected){
+                    mMenuWasInjected = false;
+                    if (DEBUG_INPUT) {
+                        Log.d(TAG, "ignoring KeyEvent.KEYCODE_MENU up event after virtual keypress");
+                    }
+                    return -1;
+                }
             }
         } else if (keyCode == KeyEvent.KEYCODE_SEARCH) {
             if (down) {
