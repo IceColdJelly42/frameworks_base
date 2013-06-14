5e856639921f50afb25566da92e8a2de05af01a9
LorD ClockaN <davor@losinj.com>
2013-06-14 21:28:44 +0200

    Framework: Collapse Shade TogglePref - AOKP
    Change-Id: I2fddec83602b284266631bdff722aa93a2d10c30
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3921,6 +3921,13 @@ public final class Settings {
         public static final String QUICK_TOGGLE_FAV_CONTACT = "quick_toggle_fav_contact";
 
         /**
+         * enable and disable shade collapse on click
+         *
+         * @hide
+         */
+        public static final String SHADE_COLLAPSE_ALL = "shade_collapse_all";
+
+        /**
          * Screenshot toggle delay
          * @hide
          */
