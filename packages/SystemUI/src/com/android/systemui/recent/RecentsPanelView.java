01be316552b96c660c4308ccf2f9d19a0246a00c
Bajee11 <bajee11@gmail.com>
2013-06-16 15:12:19 -0400

    Recents Update (WIP) - modified with icecold logo
    Conflicts:
    	packages/SystemUI/res/layout-land/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout-port/status_bar_recent_panel.xml
    	packages/SystemUI/res/layout/system_bar_recent_panel.xml

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -55,7 +55,6 @@ import android.view.animation.DecelerateInterpolator;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
