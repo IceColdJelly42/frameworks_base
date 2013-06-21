4cecfcc7eb3204253be69945745147ebc9807096
Wink Saville <wink@google.com>
2013-06-21 09:58:11 -0700

    Fix misspelling of getLteSignalStrenght.
    Change to getLteSignalStrength.
    
    Change-Id: Ie2acec9e01af0e64a61902383f06729a7cee1975

--- telephony/java/android/telephony/SignalStrength.java
+++ telephony/java/android/telephony/SignalStrength.java
@@ -414,7 +414,7 @@ public class SignalStrength implements Parcelable {
     }
 
     /** @hide */
-    public int getLteSignalStrenght() {
+    public int getLteSignalStrength() {
         return mLteSignalStrength;
     }
 
