2f967f11c43d0a744f3ca54b9a8f4c982c49c35e
LorD ClockaN <davor@losinj.com>
2013-07-14 23:31:56 +0200

    Final build fix

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/DateViewCenter.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/DateViewCenter.java
@@ -61,7 +61,7 @@ public class DateViewCenter extends TextView {
     }
 
     protected void updateClock() {
-        final String dateFormat = getContext().getString(R.string.abbrev_wday_month_day_no_year);
+        final String dateFormat = getContext().getString(R.string.full_wday_month_day_no_year_split);
         setText(DateFormat.format(dateFormat, new Date()));
     }
     
