f112559bc7265dac9b60b30d5a94faad8fd092b8
Bajee11 <bajee11@gmail.com>
2013-07-14 03:29:59 -0400

    FW: Fix torch state for QS/Power Widget
    Also add long press action for torch toggle.
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/aokp/AwesomeAction.java

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardViewManager.java
@@ -55,6 +55,7 @@ import android.widget.FrameLayout;
 
 import com.android.internal.R;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.cm.TorchConstants;
 
 /**
  * Manages creating, showing, hiding and resetting the keyguard.  Calls back
@@ -394,7 +395,7 @@ public class KeyguardViewManager {
 
     private boolean runAction(Context context, String uri) {
         if ("FLASHLIGHT".equals(uri)) {
-            context.sendBroadcast(new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT"));
+            context.sendBroadcast(new Intent(TorchConstants.ACTION_TOGGLE_STATE));
             return true;
         } else if ("NEXT".equals(uri)) {
             sendMediaButtonEvent(context, KeyEvent.KEYCODE_MEDIA_NEXT);
